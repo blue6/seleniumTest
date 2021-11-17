c8fd6d0f79b69a3d91dbd1e8d35e8b730a207b27
blue6 <blue6@blue6-no-MacBook-Pro.local>
2021-11-17 15:59:42 +0900

    update docker-compose.yml

--- src/__tests__/welcomevue.spec.js
+++ src/__tests__/welcomevue.spec.js
@@ -2,8 +2,11 @@ import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
 import { driver } from "../driver-factory";
 import WelcomeVuePage from "../__pageobjects__/welcomevuepage";
 
+const testVueUrl = process.env.TESTVUE_URL || "http://localhost:8081/";
+//const testVueUrl = "http://192.168.0.2:8080/";
+
 beforeAll(async () => {
-  await driver.navigateTo("http://localhost:8081/");
+  await driver.navigateTo(testVueUrl);
 });
 
 afterAll(async () => {
@@ -12,7 +15,8 @@ afterAll(async () => {
 
 describe("Welcome Vue page", () => {
   test("contains the correct welcome message", async () => {
-    //await driver.sleep(1000);
+    //await driver.internalDriver.sleep(1000);
+    //console.log(await driver.internalDriver.getPageSource());
 
     expect(await WelcomeVuePage.getHeadingText()).toEqual(
       "Welcome to Your Vue.js App !!!XXX"
